a0881053952d6f5b6f5e94d97f4e641e059ceb24
riyapatil <patilriya1182@gmail.com>
2025-11-07 16:17:45 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let backgroundContEl = document.getElementById("backgroundCont");
+
+let bgColorCodeEl = document.getElementById("bgColorCode");
+
+
+function colorFirst(){
+
+    backgroundContEl.style.backgroundColor = "#e0e0e0";
+    bgColorCodeEl.textContent = "Background Color : #e0e0e0";
+
+}
+
+function colorSecond(){
+
+    backgroundContEl.style.backgroundColor = "#6fcf97";
+    bgColorCodeEl.textContent = "Background Color : #6fcf97";
+
+}
+
+function colorThird(){
+
+    backgroundContEl.style.backgroundColor = "#56ccf2";
+    bgColorCodeEl.textContent = "Background Color : #56ccf2";
+
+}
+
+function colorFourth(){
+
+    backgroundContEl.style.backgroundColor = "#bb6bd9";
+    bgColorCodeEl.textContent = "Background Color : #bb6bd9";
+
+}
